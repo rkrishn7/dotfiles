544b50c66443982064648654432870e98b133f3f
rkrishn7 <rkrishn7@users.noreply.github.com>
2024-09-02 18:06:07 -0700

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,8 +1,20 @@
 set -l os (uname)
 
+# Kubernetes
+alias k="kubectl"
+alias kctx="kubectx"
+alias kns="kubens"
+
+# gimme-aws-creds
+alias gimme="gimme-aws-creds"
+alias gimmenv="eval (gimme-aws-creds -o export)"
+
 if test "$os" = Darwin
     set PATH /usr/local/bin /opt/homebrew/bin /opt/homebrew/sbin $PATH
 end
+
+set -x EDITOR nvim
+set PATH $HOME/.local/bin $HOME/.pulumi/bin $HOME/.cargo/bin $PATH
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
